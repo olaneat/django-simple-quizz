1184e0a7abf9ccdd76abc6ac5cc092fb94246f2b
Tosin <tosinayoola0@gmail.com>
2020-09-01 01:54:00 +0100

    static files

---
+++ staticfiles/js/timer.js
@@ -0,0 +1,26 @@
+document.getElementById('timer').innerHTML = 2 + ":" + 00;
+startTimer();
+
+function startTimer() {
+    var presentTime = document.getElementById('timer').innerHTML;
+    var timeArray = presentTime.split(/[:]+/);
+    var m = timeArray[0];
+    var s = checkSecond((timeArray[1] - 1));
+    if (s == 59) { m = m - 1 }
+    let finlUrl = document.getElementById('url').innerHTML;
+    if (s == 00 && m == 00) {
+        window.location.href = finlUrl
+    }
+
+
+    document.getElementById('timer').innerHTML =
+        m + ":" + s;
+    console.log(m)
+    setTimeout(startTimer, 1000);
+}
+
+function checkSecond(sec) {
+    if (sec < 10 && sec >= 0) { sec = "0" + sec };
+    if (sec < 0) { sec = "59" };
+    return sec;
+}
--- system/settings.py
+++ system/settings.py
@@ -31,7 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'story',
     'question',
-    'heroku',
+  
 
     
 ]
